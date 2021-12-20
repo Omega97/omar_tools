9cd8f0fd90ddfef2fe3b6b12ecc114a87e1387dc
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2021-12-20 17:13:18 +0100

    first upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='omar_tools',
+      version='0.9',
+      description='Useful classes and methods',
+      url='#',
+      author='Omar Cusma Fait',
+      author_email='omar.cusma.fait@gmail.com',
+      license='None',
+      packages=['omar_utils'],
+      zip_safe=False)
---
+++ test.py
@@ -0,0 +1,4 @@
+from omar_tools import i_print
+
+
+i_print([2, 3])
