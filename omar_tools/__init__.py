396d7eee79500ff55a5a8b9443ede2d88a27d22c
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2021-12-21 09:04:22 +0100

    added all imports

--- omar_tools/__init__.py
+++ omar_tools/__init__.py
@@ -1,1 +1,6 @@
-from .iter_utils import i_print
+from .debug import Debug
+from . import easyplots
+from .iter_utils import n_wise, i_print, d_print, read_file
+from .pandas_data import Data
+from . import mathematics
+from . import misc
