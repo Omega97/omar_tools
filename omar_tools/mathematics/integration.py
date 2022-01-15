654379d325ae33f4c1374830db1aadb5b0e9f12a
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2022-01-15 09:48:57 +0100

    faster integration

--- omar_tools/mathematics/integration.py
+++ omar_tools/mathematics/integration.py
@@ -1,8 +1,19 @@
+"""
+ "Integration" is a class that is useful for performing fast and precise integration in many dimensions.
+ By initializing
+
+alg = Integration(order, n_dimensions, repeat)
+
+ we may perform integration repeatedly with different functions and od different domains
+
+y1 = alg(f, v0, v1)
+y2 = alg(g, v0, v1)
+"""
 import numpy as np
 from itertools import product
 
 
-def that_matrix(size):
+def _matrix_for_finding_coefficients(size):
     m = np.zeros((size, size))
     for i in range(1, size):
         for j in range(size):
@@ -11,148 +22,79 @@ def that_matrix(size):
     return m
 
 
-def that_array(size):
+def _array_for_integration(size):
     return np.array([(size-1) ** (i + 1) / (i + 1) for i in range(size)])
-
-
-def count_interfaces(indices, order, repeat):
-    return sum(False if i in (0, order * repeat) else i % order == 0 for i in indices)
-
-
-def gamma_of_half_integer(x):
-    if x > 1:
-        return gamma_of_half_integer(x-1) * (x-1)
-    elif x == 1.:
-        return 1
-    elif x == .5:
-        return np.pi ** .5
-    else:
-        raise ValueError(f'gamma_of_half_integer({x}) is not defined')
-
-
-def n_ball_surface_area(n):
-    return np.pi ** (n/2) / gamma_of_half_integer(n/2 + 1) * n
 
 
 class BlockIntegral1D:
     """
-    1-D integration of a given order
-    call this object with function fun and extremes x0, x1 to evaluate integral
+    Integration algorithm of a given order in 1-D
+    The same set of weights is used only one once (therefore on a "block")
     """
-
     def __init__(self, order):
         assert order >= 1
         self.order = order
         self.n = 1 + self.order
         self.w = None
+
         self.compute_weights()
 
     def compute_weights(self):
-        m = that_matrix(size=self.n)
+        m = _matrix_for_finding_coefficients(size=self.n)
         m_inv = np.linalg.inv(m)
-        v = that_array(size=self.n)
+        v = _array_for_integration(size=self.n)
         self.w = v.dot(m_inv)
-
-    def __call__(self, fun, x0: float, x1: float):
-        dx = (x1 - x0) / self.order
-        return np.sum([fun(x0 + dx * i) * self.w[i] for i in range(self.n)]) * dx
 
 
 class BlockIntegralND(BlockIntegral1D):
+    """
+    Integration algorithm of a given order in N-D
+    The same set of weights is used only one once (therefore on a "block")
+    """
+    def __init__(self, order, n_dimensions):
+        super().__init__(order)
+        self.n_dimensions = n_dimensions
+        self.w_nd = None
+        self.compute_weights_nd()
 
-    def __call__(self, fun, v0: np.array, v1: np.array):
-        assert len(v0) == len(v1)
-        dim = len(v0)
-        out = 0.
-        dv = np.product(v1 - v0) / self.order ** dim
-
-        for indices in product(*(range(self.n) for _ in range(dim))):
-            x = v0 + (v1-v0) * np.array(indices) / self.order
-            w = np.product([self.w[indices[i]] for i in range(dim)])
-            out += fun(x) * w
-        return out * dv
+    def compute_weights_nd(self):
+        self.w_nd = np.zeros(shape=[self.order+1] * self.n_dimensions)
+        for j in product(*[range(self.order+1) for _ in range(self.n_dimensions)]):
+            self.w_nd.itemset(j, np.product([self.w[i] for i in j]))        # todo write better?
 
 
 class Integration(BlockIntegralND):
-
-    def __init__(self, order, repeat):
-        super().__init__(order)
+    """
+    Integration algorithm of a given order in N-D
+    The same set of weights is used repeatedly on the whole domain
+    Call this object with function fun and extremes x0, x1 to evaluate integral on a N-interval
+    """
+    def __init__(self, order, n_dimensions, repeat):
+        super().__init__(order, n_dimensions)
         self.repeat = repeat
+        self.w_int = None
+        self.compute_integration_weights()
 
-    def get_weight(self, indices, dim):
-        out = np.product([self.w[indices[i] % self.order] for i in range(dim)])
-        return out * 2 ** count_interfaces(indices, self.order, self.repeat)
+    def compute_integration_weights(self):
+        length = self.order * self.repeat + 1
+        self.w_int = np.zeros(shape=[length] * self.n_dimensions)
+
+        for i in product(*[range(self.repeat) for _ in range(self.n_dimensions)]):
+            for j in product(*[range(self.order+1) for _ in range(self.n_dimensions)]):
+                i_ = np.array(i)
+                j_ = np.array(j)
+                index_ = i_ * self.order + j_
+                self.w_int.itemset(tuple(index_), self.w_int.item(*index_) + self.w_nd.item(*j_))
 
     def __call__(self, fun, v0: np.array, v1: np.array):
-        assert len(v0) == len(v1)
-        dim = len(v0)
+        if not len(v0) == len(v1) == self.n_dimensions:
+            raise IndexError(f'v0 and v1 must have length {self.n_dimensions} ({len(v0)} and {len(v0)} found instead)')
+
         out = 0.
-        dv = np.product(v1 - v0) / self.order ** dim
+        dv = np.product(v1 - v0) / self.order ** self.n_dimensions
 
-        for weight_indices in product(*(range(self.order * self.repeat + 1) for _ in range(dim))):
-            i_ = np.array(weight_indices)
-            x = v0 + (v1-v0) * i_ / (self.order * self.repeat)
-            out += fun(x) * self.get_weight(weight_indices, dim)
+        for indices in product(*(range(self.order * self.repeat + 1) for _ in range(self.n_dimensions))):
+            x = v0 + (v1-v0) * np.array(indices) / (self.order * self.repeat)
+            out += fun(*x) * self.w_int[indices]
 
-        return out * dv / self.repeat ** dim
-
-
-class SphericalIntegration:
-
-    def __init__(self, order, repeat):
-        self.algorithm = Integration(order, repeat)
-
-    def __call__(self, fun, r0, epsilon_0, epsilon_inf, max_itr=100):
-        out = 0.
-
-        def new_f(x):
-            n = len(x)
-            return fun(x) * (x.dot(x)) ** ((n-1)/2) * n_ball_surface_area(n)
-
-        for i in range(max_itr):
-            delta_out = self.algorithm(new_f, v0=..., v1=...)
-            out += delta_out
-
-            if delta_out <= epsilon_inf:
-                return out
-        else:
-            raise ValueError('Integral does not converge!')
-
-
-def test_1():
-    alg = BlockIntegral1D(order=5)
-    print(alg.w)
-
-    out = alg(np.sin, 0, np.pi/2)
-    print(out)
-
-
-def test_2():
-    v0 = np.array([1, 3])
-    v1 = np.array([2, 4])
-    alg = BlockIntegralND(order=3)
-
-    def f(x):
-        r_2 = x.dot(x)
-        return r_2 * 3
-
-    out = alg(f, v0, v1)
-    print(out)
-
-
-def test_3():
-    v0 = np.array([0, 0])
-    v1 = np.array([4, 4])
-    alg = Integration(order=5, repeat=5)
-
-    def f(x):
-        r_2 = x.dot(x)
-        # return r_2 * 3
-        return np.exp(-r_2) / np.pi * 4
-
-    out = alg(f, v0, v1)
-    print(out)
-
-
-if __name__ == '__main__':
-    test_3()
+        return out * dv / self.repeat ** self.n_dimensions
